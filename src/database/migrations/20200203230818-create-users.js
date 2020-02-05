9a5337c220b9dd80d9e525322f7ff1d82d4e0220
Guilherme Cardoso <guilhermecardoso.info@gmail.com>
2020-02-05 13:04:23 -0300

    concluido o sistema de store do user

--- src/database/migrations/20200203230818-create-users.js
+++ src/database/migrations/20200203230818-create-users.js
@@ -7,6 +7,32 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
       },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
